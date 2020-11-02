c86716540a139c999c77d4d4c961685986c8c67f
Will Studabaker <will.studabaker@gmail.com>
2020-11-02 12:35:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,12 @@
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items
 
   def initialize(discount = 0)
     @discount = discount
     @total = 0
+  end
+
+  def add_item(title, price, quantity = 1)
+
   end
 end
